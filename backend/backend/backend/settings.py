b1b8d53bb1cc0b72d7717e89353b2983f3e66c9f
waderaku <wadebosh@icloud.com>
2021-09-12 13:19:15 +0900

    トークン発行成功

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -57,9 +57,6 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
 
 ROOT_URLCONF = "backend.urls"
 
@@ -159,3 +156,7 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
--- backend/backend/pomodoro_timer/views.py
+++ backend/backend/pomodoro_timer/views.py
@@ -3,17 +3,34 @@ from django.http import HttpResponse
 from django.http.request import HttpRequest
 from google.oauth2.credentials import Credentials
 
+from googleapiclient.discovery import build
+import datetime
+
+
+from google.auth.transport.requests import Request
+
 
 def get_task(request: HttpRequest):
     access_token = request.GET["access_token"]
     refresh_token = request.GET["refresh_token"]
-
     credentials = Credentials(
         access_token,
         refresh_token=refresh_token,
         token_uri="https://oauth2.googleapis.com/token",
         client_id="812434553636-nk0sd63psg9h3mjrqorf1jkvugglf7d8.apps.googleusercontent.com",
         client_secret="x6BHVdY60JkAt_1vyeeqPjpI",
+        scopes=["https://www.googleapis.com/auth/calendar"],
     )
+    # credentials.refresh(Request())
+
+    now = datetime.datetime.utcnow().isoformat() + "Z"
+    service = build("calendar", "v3", credentials=credentials)
+    service.events().list(
+        calendarId="primary",
+        timeMin=now,
+        maxResults=10,
+        singleEvents=True,
+        orderBy="startTime",
+    ).execute()
     return HttpResponse("success!")
 
