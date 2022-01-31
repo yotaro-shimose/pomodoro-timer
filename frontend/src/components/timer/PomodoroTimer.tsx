beab452dd8aed0382ad8ecae1b10b191eab83167
yotaro-shimose <yosemat.beta@gmail.com>
2022-01-31 15:28:07 +0900

    notification soundの実装

--- frontend/src/components/timer/PomodoroTimer.tsx
+++ frontend/src/components/timer/PomodoroTimer.tsx
@@ -12,66 +12,83 @@ import { Task } from "../../interfaces";
 import { useRecoilState } from "recoil";
 
 interface PomodoroTimerProps {
-    userId: string;
-    timerConfig: number;
-    task: Task;
+  userId: string;
+  timerConfig: number;
+  task: Task;
 }
 
-const PomodoroTimer: FC<PomodoroTimerProps> = (props) => {
-    // Modalの中身作成
-    const [open, setOpen] = useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
+const playNotificationSound = () => {
+  const player = new Audio("../notification.wav");
+  const playPromise = player.play();
 
-    const [startTime, setStartTime] = useRecoilState(startTimeState);
-    useEffect(() => {
-        setStartTime(new Date());
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
-    const time = new Date();
-    time.setMinutes(time.getMinutes() + props.timerConfig);
-    // eslint-disable-next-line
-    const { seconds, minutes, hours, days, isRunning, start, pause, resume, restart } = useTimer({
-        autoStart: true,
-        expiryTimestamp: time,
-        onExpire: handleOpen,
+  if (playPromise !== undefined) {
+    playPromise.catch((error) => {
+      console.log("playback prevented ", error);
     });
-    const dispMinutes = ('00' + minutes).slice(-2);
-    const dispSeconds = ('00' + seconds).slice(-2);
+  }
+};
 
-    const confirmFinish = confirmFinishFactory(pause, handleOpen);
-    const finishName = "finish";
+const PomodoroTimer: FC<PomodoroTimerProps> = (props) => {
+  // Modalの中身作成
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
-    const buttonDataList = [
-        {
-            func: confirmFinish,
-            buttonName: finishName,
-        },
-    ];
+  const [startTime, setStartTime] = useRecoilState(startTimeState);
+  useEffect(() => {
+    setStartTime(new Date());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    const finish = timerFinishFactory(props.userId, props.task, startTime, handleClose);
-    const cancel = cancelFinishFactory(start, handleClose);
+  const time = new Date();
+  time.setMinutes(time.getMinutes() + props.timerConfig);
 
-    return (
-        <div className="PomodoroTimer">
-            <Toolbar />
-            <Typography variant="h3">{props.task.name}</Typography>
-            <TimerScreen minutes={dispMinutes} seconds={dispSeconds} />
-            <Grid container spacing={6} alignItems="center" justifyContent="center">
-                {buttonDataList.map((buttonData, index) => (
-                    <Grid item>
-                        <TimerButton func={buttonData.func} buttonName={buttonData.buttonName} />
-                    </Grid>
-                ))}
-            </Grid>
-            <ConfirmDialog
-                open={open}
-                onClose={handleClose}
-                finishFunc={finish}
-                cancelFunc={cancel}
-            ></ConfirmDialog>
-        </div>
-    );
+  const onExpire = () => {
+    playNotificationSound();
+    handleOpen();
+  };
+
+  // eslint-disable-next-line
+  const { seconds, minutes, hours, days, isRunning, start, pause, resume, restart } = useTimer({
+    autoStart: true,
+    expiryTimestamp: time,
+    onExpire: onExpire,
+  });
+  const dispMinutes = ("00" + minutes).slice(-2);
+  const dispSeconds = ("00" + seconds).slice(-2);
+
+  const confirmFinish = confirmFinishFactory(pause, handleOpen);
+  const finishName = "finish";
+
+  const buttonDataList = [
+    {
+      func: confirmFinish,
+      buttonName: finishName,
+    },
+  ];
+
+  const finish = timerFinishFactory(props.userId, props.task, startTime, handleClose);
+  const cancel = cancelFinishFactory(start, handleClose);
+
+  return (
+    <div className="PomodoroTimer">
+      <Toolbar />
+      <Typography variant="h3">{props.task.name}</Typography>
+      <TimerScreen minutes={dispMinutes} seconds={dispSeconds} />
+      <Grid container spacing={6} alignItems="center" justifyContent="center">
+        {buttonDataList.map((buttonData, index) => (
+          <Grid item>
+            <TimerButton func={buttonData.func} buttonName={buttonData.buttonName} />
+          </Grid>
+        ))}
+      </Grid>
+      <ConfirmDialog
+        open={open}
+        onClose={handleClose}
+        finishFunc={finish}
+        cancelFunc={cancel}
+      ></ConfirmDialog>
+    </div>
+  );
 };
 export default PomodoroTimer;
--- frontend/src/factory.ts
+++ frontend/src/factory.ts
@@ -1,18 +1,24 @@
 import { pushEvent } from "./api";
 import { Task } from "./interfaces";
 import { getStringFromDate } from "./dateFormat";
-export const confirmFinishFactory = (pause: () => void, handleOpen: () => void) => () => {
-    handleOpen();
-    pause();
-}
 
-export const timerFinishFactory = (id: string, task: Task, startTime: Date, onClose: () => void) => () => {
-    const format = 'YYYY-MM-DD hh:mm:ss';
+export const confirmFinishFactory = (pause: () => void, handleOpen: () => void) => () => {
+  handleOpen();
+  pause();
+};
+
+export const timerFinishFactory =
+  (id: string, task: Task, startTime: Date, onClose: () => void) => () => {
+    const format = "YYYY-MM-DD hh:mm:ss";
     onClose();
-    pushEvent(id, { task: task, startTime: getStringFromDate(startTime, format), endTime: getStringFromDate(new Date(), format) })
-}
+    pushEvent(id, {
+      task: task,
+      startTime: getStringFromDate(startTime, format),
+      endTime: getStringFromDate(new Date(), format),
+    });
+  };
 
 export const cancelFinishFactory = (start: () => void, onClose: () => void) => () => {
-    start();
-    onClose();
-}
+  start();
+  onClose();
+};
